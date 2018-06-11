f62d9c5efdb3cb0244fb5bbd3a03aeaf804a191f
serenaliaojc <36046972+serenaliaojc@users.noreply.github.com>
2018-06-11 13:31:59 -0400

    use children

--- src/App.js
+++ src/App.js
@@ -63,7 +63,9 @@ class App extends Component {
         <Search 
           value={searchTerm}
           onChange={this.onSearchChange}
-        />
+        >
+          Search
+        </Search>
         <Table 
           list={list}
           pattern={searchTerm}
@@ -74,12 +76,31 @@ class App extends Component {
   }
 }
 
+class Button extends Component {
+  render() {
+    const {
+      onClick,
+      className,
+      children
+    } = this.props;
+
+    return (
+      <button
+        onClick={onClick}
+        className={className}
+        type="button"
+      >
+        {children}
+      </button>
+    );
+  }
+}
 class Search extends Component {
   render() {
-    const { value, onChange } = this.props;
+    const { value, onChange, children } = this.props;
     return (
       <form>
-        <input 
+        {children} <input 
           type="text"
           value={value}
           onChange={onChange}
@@ -103,12 +124,9 @@ class Table extends Component {
             <span>{item.num_comments}</span>
             <span>{item.points}</span>
             <span>
-              <button
-              onClick={() => onDismiss(item.objectID)}
-              type="button"
-              >
-              Dismiss
-              </button>
+              <Button onClick={()=>onDismiss(item.objectID)}>
+                Dismiss
+              </Button>
             </span>
           </div>
         )}
